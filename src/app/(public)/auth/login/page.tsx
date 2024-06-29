cb39adcdb6bd4de797b12e1b99cc8af50a79cdcb
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-06-29 13:24:03 +0300

    add login page

---
+++ src/app/(public)/auth/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BlockAuth } from "../ui";
+
+function LoginPage() {
+  return (
+    <BlockAuth heading="Log in" authType="Log in">
+      LOGIN
+    </BlockAuth>
+  );
+}
+
+export default LoginPage;
