06d6022e39a3218f880d4cb7802e40c82617feff
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-07-03 14:51:56 +0300

    add skeleton

--- src/context/Profile/ProfileProvider.tsx
+++ src/context/Profile/ProfileProvider.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { FC, useEffect, useState } from "react";
 
-// import { Loader, PostForm } from "@/components";
 import { useModal } from "@/hooks";
 import { IUser } from "@/types";
 import { getMe } from "@/lib/actions";
@@ -28,8 +27,6 @@ const ProfileProvider: FC<ProfileProviderProps> = ({ children }) => {
       value={{ user, setUser, setActive: postModal.setActive }}
     >
       {children}
-
-      {/* <PostForm {...postModal} user={user} enableSwipe /> */}
     </ProfileContext.Provider>
   );
 };
